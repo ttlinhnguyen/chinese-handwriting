7aaa1ef51ff8d9a32a8a9b53c5d3ef3bda642bda
Linh Nguyen <linhtunguyen.1311@gmail.com>
2025-04-16 22:58:07 +0930

    Add WordGridBox component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,33 +1,25 @@
-import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import fonts from '../config/font'
+import WordGridBox from '../components/WordGridBox';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const defaultText = "鉴于对人类家庭所有成员的固有尊严及其平等的和不移的权利的承认,乃是世界自由、正义与和平的基础"
+  const fontFaces = fonts.map(font => (
+    `
+    @font-face {
+      font-family: '${font.name}';
+      src: url('${font.url}') format('truetype');
+    }
+    `
+  ));
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <style>{fontFaces.join("\n")}</style>
+      <div style={{fontFamily: fonts[0].name}} className='flex flex-wrap text-5xl'>
+        {defaultText.split("").map(word => (
+          <WordGridBox>{word}</WordGridBox>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/components/WordGridBox.tsx
@@ -0,0 +1,9 @@
+import "./WordGridBox.css";
+
+const WordGridBox = ({...props}) => {
+    return (
+        <div className='p-1 cross-box' {...props}></div>
+    )
+}
+
+export default WordGridBox
