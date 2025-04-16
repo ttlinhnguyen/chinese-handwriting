f527937c39c81803acc24d6bcababf27cf8a4d4b
Linh Nguyen <linhtunguyen.1311@gmail.com>
2025-04-16 23:35:31 +0930

    Make the cross box line hidden under the text

--- src/components/WordGridBox.tsx
+++ src/components/WordGridBox.tsx
@@ -2,7 +2,7 @@ import "@/components/WordGridBox.css";
 
 const WordGridBox = ({...props}) => {
     return (
-        <div className='p-1 cross-box' {...props}></div>
+        <div className='cross-box' {...props}></div>
     )
 }
 
