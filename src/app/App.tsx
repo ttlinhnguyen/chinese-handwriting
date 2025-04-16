04e7f3a927afb23e20db88be1767205773147d07
Linh Nguyen <linhtunguyen.1311@gmail.com>
2025-04-16 23:23:04 +0930

    Fix cross box CSS

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,18 +4,16 @@ import WordGridBox from '@/components/WordGridBox';
 function App() {
   const defaultText = "鉴于对人类家庭所有成员的固有尊严及其平等的和不移的权利的承认,乃是世界自由、正义与和平的基础"
   const fontFaces = fonts.map(font => (
-    `
-    @font-face {
+    `@font-face {
       font-family: '${font.name}';
       src: url('${font.url}') format('truetype');
-    }
-    `
+    }`
   ));
 
   return (
     <>
       <style>{fontFaces.join("\n")}</style>
-      <div style={{fontFamily: fonts[0].name}} className='flex flex-wrap text-5xl'>
+      <div style={{fontFamily: fonts[0].name}} className='flex flex-wrap'>
         {defaultText.split("").map(word => (
           <WordGridBox>{word}</WordGridBox>
         ))}
