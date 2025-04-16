54537a0a3cf81c8fc308862680e96c79854f2e4c
Linh Nguyen <linhtunguyen.1311@gmail.com>
2025-04-16 22:56:47 +0930

    Add KaiTi font

---
+++ src/config/font.ts
@@ -0,0 +1,10 @@
+import FangZhengKaiTiJianTiURL from "@/assets/fonts/FangZhengKaiTiJianTi.ttf"
+
+const fonts = [
+    {
+        name: "FangZhengKaiTiJianTi",
+        url: FangZhengKaiTiJianTiURL,
+    }
+]
+
+export default fonts
