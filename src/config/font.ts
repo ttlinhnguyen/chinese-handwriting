692dec845b9097c1b30f4a8f8faee6c56fa4f9b9
Linh Nguyen <linhtunguyen.1311@gmail.com>
2025-04-17 12:12:27 +0930

    Add FZ fonts

--- src/config/font.ts
+++ src/config/font.ts
@@ -1,10 +1,30 @@
-import FangZhengKaiTiJianTiURL from "@/assets/fonts/FangZhengKaiTiJianTi.ttf"
+import FangZhengKaiTiGBK from "@/assets/fonts/FangZhengKaiTi-GBK.ttf"
+import FangZhengShuSongGBK from "@/assets/fonts/FangZhengShuSong-GBK.ttf"
+import FangZhengFangSongGBK from "@/assets/fonts/FangZhengFangSong-GBK.ttf"
+import FZYBXSJF from "@/assets/fonts/FZYBXSJF.ttf"
+import FZYBKSK from "@/assets/fonts/FZYBKSK.ttf"
 
 const fonts = [
     {
-        name: "FangZhengKaiTiJianTi",
-        url: FangZhengKaiTiJianTiURL,
-    }
+        name: "FZKaiTiGBK",
+        url: FangZhengKaiTiGBK,
+    },
+    {
+        name: "FZShuSongGBK",
+        url: FangZhengShuSongGBK,
+    },
+    {
+        name: "FZFangSongGBK",
+        url: FangZhengFangSongGBK,
+    },
+    {
+        name: "FZYingBiKaiShu",
+        url: FZYBKSK,
+    },
+    {
+        name: "FZYingBiXingShu",
+        url: FZYBXSJF,
+    },
 ]
 
 export default fonts
