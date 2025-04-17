15476dba2343f7a29489498761090c07028d2d1a
Linh Nguyen <linhtunguyen.1311@gmail.com>
2025-04-17 11:22:14 +0930

    Add font select input

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import fonts from '@/config/font'
 import WordGridBox from '@/components/WordGridBox';
+import { useState } from 'react';
 
 function App() {
   const defaultText = "鉴于对人类家庭所有成员的固有尊严及其平等的和不移的权利的承认,乃是世界自由、正义与和平的基础"
@@ -10,10 +11,18 @@ function App() {
     }`
   ));
 
+  const [selectedFont, setSelectedFont] = useState(fonts[0].name)
+
   return (
     <>
       <style>{fontFaces.join("\n")}</style>
-      <div style={{fontFamily: fonts[0].name}} className='flex flex-wrap'>
+      <select className='m-auto p-3' value={selectedFont} 
+        onChange={e => setSelectedFont(e.target.value)}>
+        {fonts.map(font => (
+          <option value={font.name}>{font.name}</option>
+        ))}
+      </select>
+      <div style={{fontFamily: selectedFont}} className='flex flex-wrap'>
         {defaultText.split("").map(word => (
           <WordGridBox>{word}</WordGridBox>
         ))}
