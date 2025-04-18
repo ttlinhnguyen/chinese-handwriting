c62b216386c08611ed0fe85cf21fe76b2aa880c0
Linh Nguyen <linhtunguyen.1311@gmail.com>
2025-04-18 18:45:07 +0930

    Move in-line on change events to functions

--- src/components/generator/GeneratorResult.tsx
+++ src/components/generator/GeneratorResult.tsx
@@ -18,26 +18,36 @@ const GeneratorResult: React.FC<GeneratorResultProps> = ({text}) => {
     window.print();
   };
 
+  const getEventNumberValue = (e: Event) => {
+    const target = e.target as typeof e.target & {
+      value: number;
+    };
+    return target.value;
+  };
+
+  const handleFontInput = (e: ChangeEvent<HTMLInputElement>) => setSelectedFont(e.target.value);
+  const handleSizeInput = (e: Event) => setBoxSize(getEventNumberValue(e));
+  const handleTextOpacityInput = (e: Event) => setTextOpacity(getEventNumberValue(e));
+  
+
   if (!text) return <></>;
 
   return (
     <>
       <h2 className="mt-10 mb-5">Settings</h2>
       <form className="mb-10 grid gap-5">
-        <TextField select label="Font" defaultValue={DEFAULT_FONT_NAME}
+        <TextField select size="small" label="Font" defaultValue={DEFAULT_FONT_NAME}
           slotProps={{
             input: {
               startAdornment: (
-                <InputAdornment position="start">
-                  <FontIcon />
-                </InputAdornment>
+                <InputAdornment position="start"><FontIcon /></InputAdornment>
               ),
             },
           }}
-          value={selectedFont} onChange={e => setSelectedFont(e.target.value)}>
+          value={selectedFont} onChange={handleFontInput}>
           {FONTS.map(font => (
             <MenuItem value={font.name} key={font.name} style={{fontFamily: font.name}}>
-              <span className="text-xl">{font.name}</span>
+              {font.name}
             </MenuItem>
           ))}
         </TextField>
@@ -46,13 +56,13 @@ const GeneratorResult: React.FC<GeneratorResultProps> = ({text}) => {
             <div><FormatSizeIcon /> Size</div>
             <Slider marks defaultValue={DEFAULT_BOX_SIZE} valueLabelDisplay="auto" 
               min={BOX_SIZE_THRESHOLD.min} max={BOX_SIZE_THRESHOLD.max} step={BOX_SIZE_THRESHOLD.step}
-              value={boxSize} onChange={e => setBoxSize(Number(e.target?.value))} />
+              value={boxSize} onChange={handleSizeInput} />
           </FormControl>
           <FormControl>
             <div><OpacityIcon /> Text opacity</div>
             <Slider marks defaultValue={DEFAULT_TEXT_OPACITY} valueLabelDisplay="auto" 
               min={TEXT_OPACITY_THRESHOLD.min} max={TEXT_OPACITY_THRESHOLD.max} step={TEXT_OPACITY_THRESHOLD.step}
-              value={textOpacity} onChange={e => setTextOpacity(Number(e.target?.value))} />
+              value={textOpacity} onChange={handleTextOpacityInput} />
           </FormControl>
           <div className="m-auto">
             <Button variant="contained" onClick={printResult} endIcon={<PrintIcon />}>Print</Button>
@@ -61,7 +71,7 @@ const GeneratorResult: React.FC<GeneratorResultProps> = ({text}) => {
       </form>
 
       
-      <div id="generator-result" style={{fontFamily: selectedFont}} className='flex flex-wrap gap-y-5'>
+      <div style={{fontFamily: selectedFont}} className="printable inline-flex flex-wrap gap-y-5">
         {text.split("").map((word, index) => (
           <WordGridBox key={index} text={word} size={boxSize} textOpacity={textOpacity}></WordGridBox>
         ))}
