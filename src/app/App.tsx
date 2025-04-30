8291513714f4a027595510a541e364f74c14c738
Linh Nguyen <linhtunguyen.1311@gmail.com>
2025-04-30 22:31:01 +0930

    Add chinese translation to app title

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,6 +2,7 @@ import GeneratorForm from "@/components/generator/GeneratorForm";
 import { Container } from "@mui/material";
 import { useState } from "react";
 import AppLoading from "./AppLoading";
+import { FONTS } from "@/config/font";
 
 function App() {
   const [isLoaded, setLoaded] = useState(false);
@@ -9,7 +10,12 @@ function App() {
   return (
     <>
       <nav>
-        <h1 className="text-center p-3 text-3xl">Chinese Handwriting Practice</h1>
+        <h1 className="text-center p-3">
+          <div className="text-3xl">Chinese Handwriting Practice</div>
+          <div className="text-5xl" style={{ fontFamily: FONTS[0].name }}>
+            中文手写练习
+          </div>
+        </h1>
       </nav>
       <Container maxWidth="md">{isLoaded ? <GeneratorForm /> : <AppLoading setLoaded={setLoaded} />}</Container>
     </>
